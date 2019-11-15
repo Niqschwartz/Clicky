8e6d8c637d612a45eb7a82cc8476a5baad21a537
Niq Schwartz <niqschwartz@gmail.com>
2019-11-15 17:14:48 -0600

    pictures resized so they can all fit on the page

--- clickygame/src/components/ClickItem/index.js
+++ clickygame/src/components/ClickItem/index.js
@@ -3,20 +3,15 @@ import React from "react";
 
 function ClickItem(props) {
     return (
-
-
-
-        < div
-            role="img"
-            onClick={() => props.handleClick(props.id)
-            }
-            style={{ backgroundImage: `url("${props.image}")` }}
-        />
-
-
-
-
-
+        // < div
+        //     role="img"
+        //     onClick={() => props.handleClick(props.id)
+        //     }
+        //     style={{ backgroundImage: `url("${props.image}")` }}
+        // />
+        //<div class="row">
+            <img class="col-sm-2" src={props.image} onClick={() => props.handleClick(props.id)}></img>
+        //</div>
     )
 }
 
